db873050fe88b4e787b1a038617699b5d1d60911
vlere <vlere@starlabspro.com>
2020-08-02 14:38:34 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+          <Layout>
+
+          </Layout>
+
+          <BurgerBuilder />
       </div>
     );
   }
